d15dcae4d612bf3989cc64036f05a9da0145fb95
tom hikari <tomprescott1565@gmail.com>
2022-12-04 08:12:41 +0100

    feature/redux-persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -19,16 +19,33 @@ import {
   // combineReducers,
   // ThunkAction,
 } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 
 import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import {rootReducer} from './rootReducer';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const makeStore = () =>
   configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
   });
 
+// const makeStore = () =>
+//   configureStore({
+//     reducer: rootReducer,
+//   });
+
 export const store = makeStore();
+
+export const persistor = persistStore(store);
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
